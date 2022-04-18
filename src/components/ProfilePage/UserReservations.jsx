beedfb710ba87f0a56efc315c0c09078aabded9b
mklaus2 <61567566+mklaus2@users.noreply.github.com>
2022-04-17 19:47:22 -0700

    Update UserReservations.jsx

--- src/components/ProfilePage/UserReservations.jsx
+++ src/components/ProfilePage/UserReservations.jsx
@@ -1,12 +1,100 @@
 import React, { Component, Fragment } from "react";
 import { NavLink } from "react-router-dom";
-import UserGymReservationEntry from "./UserGymReservationEntry";
-
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from '@mui/material/Box';
+import "./UserReservations.css";
+import {
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    CssBaseline,
+    makeStyles,
+    useTheme,
+    useMediaQuery,
+    Link,
+    Button,
+  } from "@material-ui/core";
 class UserReservations extends Component {
     render() { return (
-        <Fragment>
-            <UserGymReservationEntry />
-        </Fragment>
+        <>
+            <div className="GymReserveTitle">
+                <h1>Gym Reservations</h1>
+            </div>
+            <div className="CurrentPastSelection">
+            <button className="Button">Current</button>
+            <button className="Button">Past</button>
+            </div>
+            <div className="GuestPanel">  
+                <h1>Guest</h1>
+                <div className="ReservationPanel">
+                    <div className="ReserveComps">
+                        <h2>Gym Name</h2>
+                        <h2>Date/Time</h2>
+                    </div>
+                    <div className="ReserveComps">
+                        <button className="Button">View Gym</button>
+                        <button className="Button">Directions</button>
+                        <button className="Button">Manage Reservation</button>
+                    </div>
+                </div>
+                <div className="ReservationPanel">
+                    <div className="ReserveComps">
+                        <h2>Gym Name</h2>
+                        <h2>Date/Time</h2>
+                    </div>
+                    <div className="ReserveComps">
+                        <button className="Button">View Gym</button>
+                        <button className="Button">Directions</button>
+                        <button className="Button">Manage Reservation</button>
+                    </div>
+                </div>
+                <div className="ReservationPanel">
+                    <div className="ReserveComps">
+                        <h2>Gym Name</h2>
+                        <h2>Date/Time</h2>
+                    </div>
+                    <div className="ReserveComps">
+                        <button className="Button">View Gym</button>
+                        <button className="Button">Directions</button>
+                        <button className="Button">Manage Reservation</button>
+                    </div>
+                </div>
+            </div>
+            <div className="HostPanel">  
+            <h1>Host</h1>
+                <div className="ReservationPanel">
+                    <div className="ReserveComps">
+                        <h2>Gym Name</h2>
+                        <h2>Date/Time</h2>
+                    </div>
+                    <div className="ReserveComps">
+                        <button className="Button">View Gym</button>
+                        <button className="Button">Manage Reservation</button>
+                    </div>
+                </div>
+                <div className="ReservationPanel">
+                    <div className="ReserveComps">
+                        <h2>Gym Name</h2>
+                        <h2>Date/Time</h2>
+                    </div>
+                    <div className="ReserveComps">
+                        <button className="Button">View Gym</button>
+                        <button className="Button">Manage Reservation</button>
+                    </div>
+                </div>
+                <div className="ReservationPanel">
+                    <div className="ReserveComps">
+                        <h2>Gym Name</h2>
+                        <h2>Date/Time</h2>
+                    </div>
+                    <div className="ReserveComps">
+                        <button className="Button">View Gym</button>
+                        <button className="Button">Manage Reservation</button>
+                    </div>
+                </div>
+            </div>           
+        </>
     );}
 }
 export default UserReservations;

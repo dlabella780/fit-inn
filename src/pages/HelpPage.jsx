fcc998e6e895b15cc4b8c03c761d59a50deacef4
Peter Nguyen <petertainguyen@csus.edu>
2022-05-01 18:51:59 -0700

    TJ-112: Improve the area around the help page

--- src/pages/HelpPage.jsx
+++ src/pages/HelpPage.jsx
@@ -9,28 +9,27 @@ class HelpPage extends Component {
     render() { return (
         <Box sx={{
           }}>
-            <div className = "Header"
+            <div className = "Items"
                 style={{
                 }}>
+                <Typography variant="h3" style={{padding: 15, color:"black"}}>How can we help?</Typography>
+                <span id = "search-Bar"><SearchBar /></span>
+                <span id = "buttons">
+                    <Box display="flex" justifyContent="space-between">
+                        <Button component={Link} to="/pages/Hosts" variant="contained" color="primary">Hosts</Button>
+                        <Button component={Link} to="/pages/GymUsers" variant="contained" color="primary">Gym Users</Button>
+                    </Box>
+                </span>
+                <div className = "Articles"><Typography variant="h4" style={{}}>Articles</Typography></div>
+                <span id = "links">
+                    <Typography variant="h5"><NavLink to="/pages/PolicyForHosts" style={{ textDecoration: 'none' }}>Policy for Hosts</NavLink></Typography>
+                    <Typography variant="h5"><NavLink to="/pages/PolicyForGymUsers" style={{ textDecoration: 'none' }}>Policy for Gym Users</NavLink></Typography>
+                    <Typography variant="h5"><NavLink to="/pages/ContactInfo" style={{ textDecoration: 'none' }}>How can I contact Fit-Inn?</NavLink></Typography>
+                    <Typography variant="h5"><NavLink to="/pages/ProtectionAndInsurance" style={{ textDecoration: 'none' }}>Host Liability Insurance/Protection</NavLink></Typography>
+                    <Typography variant="h5"><NavLink to="/pages/CommunityGuidelines" style={{ textDecoration: 'none' }}>Community Guidelines</NavLink></Typography>
+                </span>
+                <a href='https://www.freepik.com/vectors/gym-room' style={{ textDecoration: 'none' }}>Gym room vector image created by upklyak - www.freepik.com</a>
             </div>
-            <Typography variant="h4" style={{padding: 80, color:"white"}}></Typography>
-            <Typography variant="h4" style={{padding: 15, color:"black"}}>How can we help?</Typography>
-            <span id = "search-Bar"><SearchBar /></span>
-            <span id = "buttons">
-                <Box display="flex" justifyContent="space-between">
-                    <Button component={Link} to="/pages/Hosts" variant="contained" color="primary">Hosts</Button>
-                    <Button component={Link} to="/pages/GymUsers" variant="contained" color="primary">Gym Users</Button>
-                </Box>
-            </span>
-            <Typography variant="h5" style={{}}>Articles</Typography>
-            <span id = "links">
-                <Typography variant="h6"><NavLink to="/pages/PolicyForHosts" style={{ textDecoration: 'none' }}>Policy for Hosts</NavLink></Typography>
-                <Typography variant="h6"><NavLink to="/pages/PolicyForGymUsers" style={{ textDecoration: 'none' }}>Policy for Gym Users</NavLink></Typography>
-                <Typography variant="h6"><NavLink to="/pages/ContactInfo" style={{ textDecoration: 'none' }}>How can I contact Fit-Inn?</NavLink></Typography>
-                <Typography variant="h6"><NavLink to="/pages/ProtectionAndInsurance" style={{ textDecoration: 'none' }}>Host Liability Insurance/Protection</NavLink></Typography>
-                <Typography variant="h6"><NavLink to="/pages/CommunityGuidelines" style={{ textDecoration: 'none' }}>Community Guidelines</NavLink></Typography>
-            </span>
-            <a href='https://www.freepik.com/vectors/gym-room' style={{ textDecoration: 'none' }}>Gym room vector image created by upklyak - www.freepik.com</a>
         </Box>
     );}
 }

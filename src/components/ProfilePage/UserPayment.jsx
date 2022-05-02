a7d9cbba9a394fd58a4485d79ffb1927923471a3
dlabella780 <carolinelabella@csus.edu>
2022-05-01 19:56:51 -0700

    TJ-36 - Pull information from Vendia to populate profile page.

--- src/components/ProfilePage/UserPayment.jsx
+++ src/components/ProfilePage/UserPayment.jsx
@@ -1,10 +1,29 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, Fragment } from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
-class UserPayment extends Component {
-    render() { return (
-        <Fragment>
-            <h2>This is the user's payment information.</h2>
-        </Fragment>
-    );}
+
+function UserPayment() {
+
+    const GET_USER = gql`
+        query userQuery {
+            get_User(id: "018054b3-f513-06be-ba11-5726fa2b1052") {
+                paymentMethod
+            }
+        }
+    `;
+
+    const { error, data, loading } = useQuery(GET_USER);
+
+    console.log({ loading, error, data });
+
+    if (loading) return "Loading...";
+    if (error) console.log(error);
+
+
+    return (
+        <div>
+            Payment Method: {data.get_User.paymentMethod}
+        </div>
+    );
 }
 export default UserPayment;

779afa1e4e7b7baeaf18ce6c502d2246137f6034
dlabella780 <carolinelabella@csus.edu>
2022-09-19 01:15:38 -0700

    Addressing an incorrect API key being listed as public.

--- src/components/PaymentForm/StripeContainer.jsx
+++ src/components/PaymentForm/StripeContainer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import PaymentForm from "./PaymentForm.jsx";
 
 
-const PUBLIC_KEY = "sk_test_51LjZ0MHSMtfvYBv6qgdxNEbqdRje9c0c8ttFqJdbhIBWDrXf5NDB2swPwjf7Mdb8PjCkGNAjSCXWPjn7vVwrNoUV00tSQFXs8j";
+const PUBLIC_KEY = "pk_test_51LjZ0MHSMtfvYBv6DzWueAsBhltM9JDeLSZGfifoFVXTT5ugkU6fbMb0x4f4VVugcgJ9hh3P8EkTiJrvIi2EyRe1002tCfXrIL";
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY)
 

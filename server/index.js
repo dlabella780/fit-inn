14d87c5f5ffa9736ed7bcc77eb4850ec9db1d1f8
brettd6 <79177376+brettd6@users.noreply.github.com>
2022-09-21 11:09:41 -0700

    TJ-130 added node server

---
+++ server/index.js
@@ -0,0 +1,133 @@
+import { request, GraphQLClient, gql } from 'graphql-request';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { query } from 'express';
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const graphQLClient = new GraphQLClient(process.env.GRAPHQL_ENDPOINT, {
+headers: {
+    authorization: process.env.API_KEY,
+},
+});
+
+const equipQuery = gql`
+query equipQuery {
+    list_EquipmentItems {
+      _EquipmentItems {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+const gymMutation = gql`
+		mutation gymMutation(
+			$accessInformation: String = "", 
+			$address: String = "", 
+			$availability: [String] = "", 
+			$bookingNotice: Int = 3, 
+			$cancelationWarning: Int = 24, 
+			$cost: Int = 20, 
+			$description: String = "", 
+			$tvType: String = "", 
+			$rating: Int = 0, 
+			$title: String = "", 
+			$photos: [String] = "", 
+			$ownerId: String = "", 
+			$numGuestsAllowed: Int = 2, 
+			$isHostHome: Boolean = false, 
+			$isActive: Boolean = false, 
+			$hasWifi: Boolean = false, 
+			$hasSpeakers: Boolean = false, 
+			$hasBathroom: Boolean = false,
+			$equipment: [Self_Gym_equipment_equipmentItem_Input_] = {}
+		){
+		add_Gym(
+		input: {
+			accessInformation: $accessInformation, 
+			address: $address, 
+			availability: $availability, 
+			bookingNotice: $bookingNotice, 
+			cancelationWarning: $cancelationWarning, 
+			cost: $cost, 
+			description: $description, 
+			hasBathroom: $hasBathroom, 
+			hasSpeakers: $hasSpeakers, 
+			isActive: $isActive, 
+			hasWifi: $hasWifi, 
+			isHostHome: $isHostHome, 
+			numGuestsAllowed: $numGuestsAllowed, 
+			ownerId: $ownerId, 
+			photos: $photos, 
+			rating: $rating, 
+			title: $title, 
+			tvType: $tvType, 
+			equipment: $equipment}
+		syncMode: NODE_LEDGERED
+		) {
+			transaction {
+				_id
+				submissionTime
+			}
+			result {
+				_id
+			}
+    	}
+	}`;
+
+
+
+app.get('/api/listEquipment', async (req,res) => {
+    if (req.get('origin') === process.env.CLIENT_URL || req.get('origin') === process.env.CLIENT_URL_SECURE) {
+      const results = await graphQLClient.request(equipQuery);
+      res.send(results);
+    }
+    else res.send('Access Denied.');
+    
+})
+
+app.post('/api/uploadGym', async (req, res) => {
+
+  if(req.get('origin') === process.env.CLIENT_URL || req.get('origin') === process.env.CLIENT_URL_SECURE) {
+    const variables = {
+      accessInformation: req.body.accessInformation, 
+      address: req.body.address, 
+      availability: req.body.availability, 
+      bookingNotice: req.body.bookingNotice, 
+      cancelationWarning: req.body.cancelationWarning, 
+      cost: req.body.cost, 
+      description: req.body.description, 
+      hasBathroom: req.body.hasBathroom, 
+      hasSpeakers: req.body.hasSpeakers, 
+      isActive: req.body.isActive, 
+      hasWifi: req.body.hasWifi, 
+      isHostHome: req.body.isHostHome, 
+      numGuestsAllowed: req.body.numGuestsAllowed, 
+      ownerId: req.body.ownerId, 
+      photos: req.body.photos, 
+      rating: req.body.rating, 
+      title: req.body.title, 
+      tvType: req.body.tvType, 
+      equipment: req.body.equipment
+    }
+
+    const data = await graphQLClient.request(gymMutation, variables)
+      
+  }
+  else res.send('Access Denied.');
+})
+
+app.listen(3001, () =>  {
+    console.log(`Server running on port 3001`);
+});

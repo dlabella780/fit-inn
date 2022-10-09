9ef1873b38727184b90b7fd40b3212b684a71151
caldwell-matthew <caldwell.j.matthew@gmail.com>
2022-10-09 05:33:24 -0400

    Minor tweaks

--- client/src/components/GymSearch/GymThumbnail.jsx
+++ client/src/components/GymSearch/GymThumbnail.jsx
@@ -2,24 +2,22 @@ import React from "react";
 
 const GymThumbnail = (props) => {
     if (props.loading) return "Loading...";
-    else{
-        return ( <div> 
-            {props.gymData.list_GymItems._GymItems.map((val) => {
-                return( <>
-                        Title: {val.title}<br/>
-                        Cost: {val.cost}<br/>
-                        Address: <br/>
-                        Street1: {val.address.street1} <br/>
-                        Street2: {val.address.stree2} <br/>
-                        City: {val.address.City} <br/>
-                        State: {val.address.State} <br/>
-                        Country: {val.address.Country} <br/>
-                        Zipcode: {val.address.zipcode} <br/>
-                        Rating: {val.rating} Stars<br/>
-                </> )
-            })}
-            {console.log(props.gymData)}
-        </div> );
-    }
+    else { return ( <div> 
+        {props.gymData.list_GymItems._GymItems.map((val) => {
+            return( <>
+                Title: {val.title}<br/>
+                Cost: {val.cost}<br/>
+                Address: <br/>
+                Street1: {val.address.street1} <br/>
+                Street2: {val.address.stree2} <br/>
+                City: {val.address.City} <br/>
+                State: {val.address.State} <br/>
+                Country: {val.address.Country} <br/>
+                Zipcode: {val.address.zipcode} <br/>
+                Rating: {val.rating} Stars<br/>
+            </> )
+        })}
+        {console.log(props.gymData)}
+    </div> );}
 }
 export default GymThumbnail;

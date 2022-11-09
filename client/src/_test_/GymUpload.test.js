6e0432edae9794ecba1c016e0c6b385200b30c64
caldwell-matthew <caldwell.j.matthew@gmail.com>
2022-11-09 18:59:05 -0500

    First 4 tests completed and passed.

--- client/src/_test_/GymUpload.test.js
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import UploadTab from '../components/GymUpload/UploadTab';
-configure({ adapter: new Adapter() });
-
-it("renders UploadTab with all components", () => {
-    shallow(<UploadTab />);
-});
